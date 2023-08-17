74e4650064b5e21b92632be4910396e9d1de7095
Md. Rasel Mandol <130071257+raselmandol@users.noreply.github.com>
2023-08-17 23:48:23 +0530

    Create diskDetailsWin.cpp

---
+++ disk/diskDetailsWin.cpp
@@ -0,0 +1,128 @@
+#include <iostream>
+#include <comdef.h>
+#include <Wbemidl.h>
+
+#pragma comment(lib, "wbemuuid.lib")
+int main() {
+    HRESULT hres;
+    hres = CoInitializeEx(0, COINIT_MULTITHREADED);
+    if (FAILED(hres)) {
+        std::cerr << "Failed to initialize COM library" << std::endl;
+        return 1;
+    }
+    hres = CoInitializeSecurity(
+        nullptr,
+        -1,
+        nullptr,
+        nullptr,
+        RPC_C_AUTHN_LEVEL_DEFAULT,
+        RPC_C_IMP_LEVEL_IMPERSONATE,
+        nullptr,
+        EOAC_NONE,
+        nullptr
+    );
+    if (FAILED(hres)) {
+        std::cerr << "Failed to initialize security" << std::endl;
+        CoUninitialize();
+        return 1;
+    }
+    IWbemLocator* pLoc = nullptr;
+    hres = CoCreateInstance(
+        CLSID_WbemLocator,
+        nullptr,
+        CLSCTX_INPROC_SERVER,
+        IID_IWbemLocator,
+        reinterpret_cast<LPVOID*>(&pLoc)
+    );
+    if (FAILED(hres)) {
+        std::cerr << "Failed to create IWbemLocator object" << std::endl;
+        CoUninitialize();
+        return 1;
+    }
+    IWbemServices* pSvc = nullptr;
+    hres = pLoc->ConnectServer(
+        _bstr_t(L"ROOT\\CIMV2"),
+        nullptr,
+        nullptr,
+        0,
+        nullptr,
+        0,
+        0,
+        &pSvc
+    );
+    if (FAILED(hres)) {
+        std::cerr << "Failed to connect to WMI" << std::endl;
+        pLoc->Release();
+        CoUninitialize();
+        return 1;
+    }
+    hres = CoSetProxyBlanket(
+        pSvc,
+        RPC_C_AUTHN_WINNT,
+        RPC_C_AUTHZ_NONE,
+        nullptr,
+        RPC_C_AUTHN_LEVEL_CALL,
+        RPC_C_IMP_LEVEL_IMPERSONATE,
+        nullptr,
+        EOAC_NONE
+    );
+    if (FAILED(hres)) {
+        std::cerr << "Failed to set proxy blanket" << std::endl;
+        pSvc->Release();
+        pLoc->Release();
+        CoUninitialize();
+        return 1;
+    }
+    IEnumWbemClassObject* pEnumerator = nullptr;
+    hres = pSvc->ExecQuery(
+        bstr_t("WQL"),
+        bstr_t("SELECT * FROM Win32_DiskDrive"),
+        WBEM_FLAG_FORWARD_ONLY | WBEM_FLAG_RETURN_IMMEDIATELY,
+        nullptr,
+        &pEnumerator
+    );
+    if (FAILED(hres)) {
+        std::cerr << "Failed to execute query" << std::endl;
+        pSvc->Release();
+        pLoc->Release();
+        CoUninitialize();
+        return 1;
+    }
+    IWbemClassObject* pclsObj = nullptr;
+    ULONG uReturn = 0;
+    while (pEnumerator) {
+        hres = pEnumerator->Next(WBEM_INFINITE, 1, &pclsObj, &uReturn);
+        if (0 == uReturn) {
+            break;
+        }
+        VARIANT vtProp;
+        hres = pclsObj->Get(L"Model", 0, &vtProp, nullptr, nullptr);
+        if (SUCCEEDED(hres)) {
+            std::wcout << "Disk Model: " << vtProp.bstrVal << std::endl;
+            VariantClear(&vtProp);
+        }
+
+        pclsObj->Release();
+    }
+    hres = pSvc->ExecQuery(
+        bstr_t("WQL"),
+        bstr_t("SELECT * FROM Win32_LogicalDisk"),
+        WBEM_FLAG_FORWARD_ONLY | WBEM_FLAG_RETURN_IMMEDIATELY,
+        nullptr,
+        &pEnumerator
+    );
+    if (FAILED(hres)) {
+        std::cerr << "Failed to execute query" << std::endl;
+        pSvc->Release();
+        pLoc->Release();
+        CoUninitialize();
+        return 1;
+    }
+    while (pEnumerator) {
+        hres = pEnumerator->Next(WBEM_INFINITE, 1, &pclsObj, &uReturn);
+        if (0 == uReturn) {
+            break;
+        }
+        VARIANT vtProp;
+        hres = pclsObj->Get(L"DeviceID", 0, &vtProp, nullptr, nullptr);
+        if (
