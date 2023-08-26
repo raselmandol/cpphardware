3654d5edda5e8ce47adac2f1f96909eb762ddbbb
Md. Rasel Mandol <130071257+raselmandol@users.noreply.github.com>
2023-08-26 19:26:24 +0530

    Create macOS.cpp

---
+++ USB/macOS.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+int main() {
+    std::ifstream sysInfoFile("/usr/sbin/system_profiler SPUSBDataType");
+    if (!sysInfoFile.is_open()) {
+        std::cerr << "Failed to open system profiler" << std::endl;
+        return 1;
+    }
+
+    std::stringstream buffer;
+    buffer << sysInfoFile.rdbuf();
+    std::string sysInfo = buffer.str();
+
+    std::cout << sysInfo;
+
+    sysInfoFile.close();
+    return 0;
+}
