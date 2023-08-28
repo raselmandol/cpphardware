6a77f87b128bc3c482cb826c60a617fa39a78ed8
Md. Rasel Mandol <130071257+raselmandol@users.noreply.github.com>
2023-08-28 23:31:39 +0530

    Create macOS.cpp

---
+++ BluetoothDevices/macOS.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <CoreBluetooth/CoreBluetooth.h>
+
+int main() {
+    @autoreleasepool {
+        CBCentralManager* centralManager = [[CBCentralManager alloc] initWithDelegate:nil queue:nil];
+        [centralManager scanForPeripheralsWithServices:nil options:nil];
+        sleep(5);
+
+        NSArray* peripherals = [centralManager retrievePeripheralsWithIdentifiers:@[]];
+        for (CBPeripheral* peripheral in peripherals) {
+            NSLog(@"Bluetooth Peripheral Name: %@", peripheral.name);
+        }
+    }
+    return 0;
+}
