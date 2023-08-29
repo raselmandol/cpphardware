b3765488357a6e16f5c8a0e01495d99f222067bc
Md. Rasel Mandol <130071257+raselmandol@users.noreply.github.com>
2023-08-29 23:34:19 +0530

    Create LEDLinux.cpp

---
+++ Microcontrollers/LEDLinux.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <fstream>
+#include <unistd.h>
+
+int main() {
+    const std::string gpioPath = "/sys/class/gpio/gpio17";
+    std::ofstream exportFile("/sys/class/gpio/export");
+
+    if (!exportFile.is_open()) {
+        std::cerr << "Error: Unable to export GPIO pin." << std::endl;
+        return 1;
+    }
+
+    exportFile << "17";
+    exportFile.close();
+
+    std::ofstream directionFile(gpioPath + "/direction");
+    if (!directionFile.is_open()) {
+        std::cerr << "Error: Unable to set GPIO direction." << std::endl;
+        return 1;
+    }
+
+    directionFile << "out";
+    directionFile.close();
+
+    while (true) {
+        std::ofstream valueFile(gpioPath + "/value");
+        if (!valueFile.is_open()) {
+            std::cerr << "Error: Unable to set GPIO value." << std::endl;
+            return 1;
+        }
+
+        valueFile << "1";
+        valueFile.close();
+        usleep(1000000); 
+
+        valueFile.open(gpioPath + "/value");
+        if (!valueFile.is_open()) {
+            std::cerr << "Error: Unable to set GPIO value." << std::endl;
+            return 1;
+        }
+
+        valueFile << "0"; 
+        valueFile.close();
+        usleep(1000000); 
+    }
+
+    return 0;
+}
