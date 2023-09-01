243dea7793b0e36f5978505a5040f86dd42309a2
Md. Rasel Mandol <130071257+raselmandol@users.noreply.github.com>
2023-09-01 21:44:58 +0530

    Create Linux.cpp

---
+++ NFC/Linux.cpp
@@ -0,0 +1,19 @@
+//using libnfc
+#include <iostream>
+#include <nfc/nfc.h>
+
+int main() {
+    nfc_context* context;
+    nfc_init(&context);
+
+    //Open the first NFC device
+    nfc_device* pnd = nfc_open(context, NULL);
+    if (pnd != NULL) {
+        //Configure and interact with the NFC device
+        //For example, you can read NFC tags or send commands
+        nfc_close(pnd);
+    }
+
+    nfc_exit(context);
+    return 0;
+}
