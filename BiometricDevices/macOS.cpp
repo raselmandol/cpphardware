18c47fdf2657dcc2356a10a4b4cab363878c060d
Md. Rasel Mandol <130071257+raselmandol@users.noreply.github.com>
2023-09-02 23:38:06 +0530

    Create macOS.cpp

---
+++ BiometricDevices/macOS.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#import <LocalAuthentication/LocalAuthentication.h>
+
+int main() {
+    LAContext* context = [[LAContext alloc] init];
+    NSError* error = nil;
+    
+    if ([context canEvaluatePolicy:LAPolicyDeviceOwnerAuthenticationWithBiometrics error:&error]) {
+        [context evaluatePolicy:LAPolicyDeviceOwnerAuthenticationWithBiometrics
+                localizedReason:@"Authenticate using biometrics"
+                          reply:^(BOOL success, NSError* error) {
+            if (success) {
+                //Biometric authentication successful
+            } else {
+                //Biometric authentication failed or was canceled
+            }
+        }];
+    } else {
+        //Biometrics not available on this device
+    }
+
+    return 0;
+}
