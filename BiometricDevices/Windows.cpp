c186e6bdd687d77aad3b6fe158bc7217afa4ced3
Md. Rasel Mandol <130071257+raselmandol@users.noreply.github.com>
2023-09-02 23:37:37 +0530

    Create Windows.cpp

---
+++ BiometricDevices/Windows.cpp
@@ -0,0 +1,17 @@
+#include <windows.h>
+#include <winbio.h>
+
+int main() {
+    HRESULT hr = S_OK;
+    WINBIO_SESSION_HANDLE sessionHandle = NULL;
+    
+    hr = WinBioOpenSession(WINBIO_TYPE_FINGERPRINT, WINBIO_POOL_SYSTEM, WINBIO_FLAG_RAW, NULL, 0, 0, &sessionHandle);
+    if (FAILED(hr)) {
+        //Handle error
+    }
+
+    //Enumerate biometric units, capture data, etc.
+
+    WinBioCloseSession(sessionHandle);
+    return 0;
+}
