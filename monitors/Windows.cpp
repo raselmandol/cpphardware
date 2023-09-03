8caf15627269f14a953631c3212dd8ab30a1d884
Md. Rasel Mandol <130071257+raselmandol@users.noreply.github.com>
2023-09-03 22:43:07 +0530

    Create Windows.cpp

---
+++ monitors/Windows.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <windows.h>
+
+int main() {
+    DISPLAY_DEVICE displayDevice;
+    displayDevice.cb = sizeof(DISPLAY_DEVICE);
+    
+    for (int i = 0; EnumDisplayDevices(NULL, i, &displayDevice, 0); i++) {
+        if (displayDevice.StateFlags & DISPLAY_DEVICE_ATTACHED_TO_DESKTOP) {
+            std::cout << "Device Name: " << displayDevice.DeviceName << std::endl;
+            std::cout << "Device String: " << displayDevice.DeviceString << std::endl;
+            //Print other display details as needed
+        }
+    }
+
+    return 0;
+}
