b34878e5a37c1c68929a29958831ce63815b2a7c
Md. Rasel Mandol <130071257+raselmandol@users.noreply.github.com>
2023-09-04 22:05:29 +0530

    Create Linux.cpp

---
+++ SoundCard/Linux.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <alsa/asoundlib.h>
+
+int main() {
+    int card = -1;
+    snd_ctl_t* handle;
+
+    while (snd_card_next(&card) == 0 && card >= 0) {
+        char cardName[32];
+        sprintf(cardName, "hw:%d", card);
+
+        if (snd_ctl_open(&handle, cardName, 0) >= 0) {
+            printf("Sound Card %d\n", card);
+            printf("Card Name: %s\n", snd_ctl_card_info_get_name(handle));
+            printf("Card ID: %s\n", snd_ctl_card_info_get_id(handle));
+            printf("Card Driver: %s\n", snd_ctl_card_info_get_driver(handle));
+            //Print other sound card details as needed
+            snd_ctl_close(handle);
+        }
+    }
+
+    return 0;
+}
