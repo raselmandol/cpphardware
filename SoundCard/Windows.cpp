fca9d9e8487f9dfe1176bcd71b95c2ed779de800
Md. Rasel Mandol <130071257+raselmandol@users.noreply.github.com>
2023-09-04 22:05:05 +0530

    Create Windows.cpp

---
+++ SoundCard/Windows.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <windows.h>
+#include <mmsystem.h>
+
+int main() {
+    int deviceCount = waveOutGetNumDevs();
+    
+    for (int i = 0; i < deviceCount; i++) {
+        WAVEOUTCAPS waveOutCaps;
+        if (waveOutGetDevCaps(i, &waveOutCaps, sizeof(waveOutCaps)) == MMSYSERR_NOERROR) {
+            printf("Sound Card %d\n", i + 1);
+            printf("Device Name: %s\n", waveOutCaps.szPname);
+            printf("Manufacturer ID: %d\n", waveOutCaps.wMid);
+            printf("Product ID: %d\n", waveOutCaps.wPid);
+            printf("Channels: %d\n", waveOutCaps.wChannels);
+            printf("Support: 0x%X\n", waveOutCaps.dwSupport);
+            //Print other sound card details as needed
+        }
+    }
+
+    return 0;
+}
