46702762c9c9d484e33d649ccf6cd14aa926750d
Md. Rasel Mandol <130071257+raselmandol@users.noreply.github.com>
2023-09-07 21:19:25 +0530

    Create Windows.cpp

---
+++ touchpad/Windows.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <windows.devices.input.h>
+#include <wrl.h>
+
+using namespace Windows::Devices::Input;
+using namespace Microsoft::WRL;
+using namespace Microsoft::WRL::Wrappers;
+
+int main() {
+    RoInitializeWrapper initialize(RO_INIT_MULTITHREADED);
+    if (FAILED(initialize)) {
+        std::cerr << "Failed to initialize Windows Runtime" << std::endl;
+        return 1;
+    }
+
+    ComPtr<MouseCapabilities> mouseCapabilities;
+    HRESULT hr = GetActivationFactory(HStringReference(L"Windows.Devices.Input.MouseCapabilities").Get(), &mouseCapabilities);
+
+    if (SUCCEEDED(hr)) {
+        uint32_t numberOfButtons;
+        hr = mouseCapabilities->get_NumberOfButtons(&numberOfButtons);
+
+        if (SUCCEEDED(hr)) {
+            std::wcout << L"Number of Mouse Buttons: " << numberOfButtons << std::endl;
+        }
+    }
+
+    return 0;
+}
