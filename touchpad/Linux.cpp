aa45d561475291a0e5f312be1d2f5535f8f67594
Md. Rasel Mandol <130071257+raselmandol@users.noreply.github.com>
2023-09-07 21:20:02 +0530

    Create Linux.cpp

---
+++ touchpad/Linux.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <libinput.h>
+
+int main() {
+    struct libinput* li;
+    struct libinput_event* event;
+
+    li = libinput_path_create_context(&interface, NULL);
+
+    if (li == NULL) {
+        fprintf(stderr, "Failed to create libinput context\n");
+        return 1;
+    }
+
+    while (1) {
+        libinput_dispatch(li);
+
+        while ((event = libinput_get_event(li))) {
+            switch (libinput_event_get_type(event)) {
+                case LIBINPUT_EVENT_DEVICE_ADDED:
+                    printf("Touchpad Device Added\n");
+                    break;
+                default:
+                    break;
+            }
+            libinput_event_destroy(event);
+        }
+    }
+
+    return 0;
+}
