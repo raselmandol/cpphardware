b124c74620c6ae1ed2c370d3f9839c5461bfaccb
Md. Rasel Mandol <130071257+raselmandol@users.noreply.github.com>
2023-09-08 23:35:34 +0530

    Create macOS.cpp

---
+++ Ethernet/macOS.cpp
@@ -0,0 +1,21 @@
+#include <SystemConfiguration/SystemConfiguration.h>
+#include <stdio.h>
+
+int main() {
+    SCNetworkReachabilityRef reachability = SCNetworkReachabilityCreateWithName(kCFAllocatorDefault, "www.apple.com");
+
+    if (reachability) {
+        SCNetworkConnectionFlags flags;
+        if (SCNetworkReachabilityGetFlags(reachability, &flags)) {
+            if ((flags & kSCNetworkFlagsReachable) && !(flags & kSCNetworkFlagsConnectionRequired)) {
+                printf("Connected to a network (possibly Ethernet).\n");
+            } else {
+                printf("Not connected to a network.\n");
+            }
+        }
+
+        CFRelease(reachability);
+    }
+
+    return 0;
+}
