42fd96bfdacdb44bb47f7a1bdb8c2c926f7e2e20
Md. Rasel Mandol <130071257+raselmandol@users.noreply.github.com>
2023-09-08 23:34:53 +0530

    Create Windows.cpp

---
+++ Ethernet/Windows.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <windows.networking.connectivity.h>
+#include <wrl.h>
+
+using namespace Windows::Networking::Connectivity;
+using namespace Microsoft::WRL;
+using namespace Microsoft::WRL::Wrappers;
+
+int main() {
+    RoInitializeWrapper initialize(RO_INIT_MULTITHREADED);
+    if (FAILED(initialize)) {
+        std::cerr << "Failed to initialize Windows Runtime" << std::endl;
+        return 1;
+    }
+
+    NetworkInformation::GetInternetConnectionProfile();
+
+    if (NetworkInformation::GetInternetConnectionProfile()) {
+        std::cout << "Connected to a network (possibly Ethernet)." << std::endl;
+    } else {
+        std::cout << "Not connected to a network." << std::endl;
+    }
+
+    return 0;
+}
