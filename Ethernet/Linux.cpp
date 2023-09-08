e4ad9bc9acb82a896c5adb0b1464898b687f7ed5
Md. Rasel Mandol <130071257+raselmandol@users.noreply.github.com>
2023-09-08 23:35:11 +0530

    Create Linux.cpp

---
+++ Ethernet/Linux.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/ioctl.h>
+#include <net/if.h>
+
+int main() {
+    int sock = socket(AF_INET, SOCK_DGRAM, 0);
+
+    if (sock == -1) {
+        perror("Failed to create socket");
+        return 1;
+    }
+
+    struct ifreq ifr;
+    strcpy(ifr.ifr_name, "eth0");
+
+    if (ioctl(sock, SIOCGIFFLAGS, &ifr) != -1 && (ifr.ifr_flags & IFF_UP)) {
+        printf("Connected to a network (possibly Ethernet).\n");
+    } else {
+        printf("Not connected to a network.\n");
+    }
+
+    close(sock);
+    return 0;
+}
