755a09bc5d0be2d91d4801c01742857ceaf03f45
Md. Rasel Mandol <130071257+raselmandol@users.noreply.github.com>
2023-09-12 23:41:11 +0530

    Create Windows.cpp

---
+++ batteryHealth/Windows.cpp
@@ -0,0 +1,23 @@
+//remember
+//no direct API for Windows
+#include <iostream>
+#include <Windows.h>
+
+int main() {
+    SYSTEM_POWER_STATUS powerStatus;
+    if (GetSystemPowerStatus(&powerStatus)) {
+        double batteryCapacity = powerStatus.BatteryLifePercent;
+        
+        if (batteryCapacity >= 80.0) {
+            std::cout << "Battery Health: Good" << std::endl;
+        } else if (batteryCapacity >= 60.0) {
+            std::cout << "Battery Health: Fair" << std::endl;
+        } else {
+            std::cout << "Battery Health: Poor" << std::endl;
+        }
+    } else {
+        std::cerr << "Failed to retrieve power status." << std::endl;
+    }
+
+    return 0;
+}
