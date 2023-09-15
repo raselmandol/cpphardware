09b5285d9630fb92967c8bf0fcde51e4756c4564
Md. Rasel Mandol <130071257+raselmandol@users.noreply.github.com>
2023-09-15 21:04:02 +0530

    Create Window.cpp

---
+++ GPUinDetailed/Window.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <windows.h>
+#include <dxgi.h>
+
+int main() {
+    IDXGIFactory* pFactory;
+    HRESULT hr = CreateDXGIFactory(__uuidof(IDXGIFactory), (void**)&pFactory);
+
+    if (SUCCEEDED(hr)) {
+        IDXGIAdapter* pAdapter;
+        if (pFactory->EnumAdapters(0, &pAdapter) == S_OK) {
+            DXGI_ADAPTER_DESC adapterDesc;
+            if (SUCCEEDED(pAdapter->GetDesc(&adapterDesc))) {
+                std::wcout << L"GPU Description: " << adapterDesc.Description << std::endl;
+                std::wcout << L"GPU Vendor ID: " << adapterDesc.VendorId << std::endl;
+                //more GPU information in adapterDesc-->possible
+
+                pAdapter->Release();
+            }
+        }
+
+        pFactory->Release();
+    }
+
+    return 0;
+}
