234550cebf5834de7e73bdcf6a0a556cdebf4bcc
Md. Rasel Mandol <130071257+raselmandol@users.noreply.github.com>
2023-09-16 21:28:43 +0530

    Create Linux.cpp

---
+++ DetailedRam/Linux.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+int main() {
+    std::ifstream meminfo("/proc/meminfo");
+    std::string line;
+
+    while (std::getline(meminfo, line)) {
+        if (line.find("MemTotal:") != std::string::npos) {
+            size_t pos = line.find(":");
+            if (pos != std::string::npos) {
+                std::string ramSize = line.substr(pos + 2); // Skip ": "
+                std::cout << "Total RAM: " << ramSize << std::endl;
+                break;
+            }
+        }
+    }
+
+    meminfo.close();
+    return 0;
+}
