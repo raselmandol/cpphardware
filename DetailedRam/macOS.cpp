e0ed4e51801805b0c3774b20b106c80e18c52d29
Md. Rasel Mandol <130071257+raselmandol@users.noreply.github.com>
2023-09-16 21:29:02 +0530

    Create macOS.cpp

---
+++ DetailedRam/macOS.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <sys/types.h>
+#include <sys/sysctl.h>
+
+int main() {
+    int mib[2];
+    mib[0] = CTL_HW;
+    mib[1] = HW_MEMSIZE;
+    uint64_t ramSize;
+    size_t length = sizeof(ramSize);
+
+    if (sysctl(mib, 2, &ramSize, &length, nullptr, 0) == 0) {
+        std::cout << "Total RAM: " << ramSize << " bytes" << std::endl;
+    } else {
+        std::cerr << "Failed to retrieve RAM information." << std::endl;
+    }
+
+    return 0;
+}
