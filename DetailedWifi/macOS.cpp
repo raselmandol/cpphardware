081c34b5da87217382a332de2fd20a111701e0e6
Md. Rasel Mandol <130071257+raselmandol@users.noreply.github.com>
2023-09-19 22:10:00 +0530

    Create macOS.cpp

---
+++ DetailedWifi/macOS.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <CoreFoundation/CoreFoundation.h>
+#include <SystemConfiguration/SCDynamicStore.h>
+#include <SystemConfiguration/SCSchemaDefinitions.h>
+
+int main() {
+    SCDynamicStoreRef store = SCDynamicStoreCreate(kCFAllocatorDefault, CFSTR("WifiInfo"), nullptr, nullptr);
+    if (!store) {
+        std::cerr << "Failed to create SCDynamicStore." << std::endl;
+        return 1;
+    }
+    CFPropertyListRef info = SCDynamicStoreCopyValue(store, CFSTR("State:/Network/Interface/en0/AirPort"));
+    if (!info) {
+        std::cerr << "Failed to retrieve WiFi info." << std::endl;
+        CFRelease(store);
+        return 1;
+    }
+    CFShow(info); 
+    CFRelease(info);
+    CFRelease(store);
+
+    return 0;
+}
