b73173abf840c4ead6a92d247a238ff43746ee6f
Md. Rasel Mandol <130071257+raselmandol@users.noreply.github.com>
2023-09-20 23:23:40 +0530

    Create macOS.cpp

---
+++ detailedPower/macOS.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <IOKit/IOKitLib.h>
+
+int main() {
+    CFTypeRef powerInfo = IOPSCopyPowerSourcesInfo();
+    CFArrayRef powerSources = IOPSCopyPowerSourcesList(powerInfo);
+
+    if (powerSources) {
+        CFDictionaryRef powerSource = static_cast<CFDictionaryRef>(CFArrayGetValueAtIndex(powerSources, 0));
+        if (powerSource) {
+            std::cout << "Battery Present: " << (CFDictionaryContainsKey(powerSource, CFSTR(kIOPSIsPresent)) ? "Yes" : "No") << std::endl;
+            std::cout << "Battery Charging: " << (CFDictionaryContainsKey(powerSource, CFSTR(kIOPSIsCharging)) ? "Yes" : "No") << std::endl;
+        }
+
+        CFRelease(powerSources);
+    } else {
+        std::cerr << "Failed to retrieve power source information." << std::endl;
+    }
+
+    if (powerInfo) {
+        CFRelease(powerInfo);
+    }
+
+    return 0;
+}
