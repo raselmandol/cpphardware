3c44c5749c59ea01806910261d0edeec2fca6743
Md. Rasel Mandol <130071257+raselmandol@users.noreply.github.com>
2023-09-26 21:56:46 +0530

    Create Linux.cpp

---
+++ Firmware/Linux.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstdio>
+
+int main() {
+    FILE* fp = popen("dmidecode -t bios", "r");
+    if (!fp) {
+        std::cerr << "Failed to execute dmidecode" << std::endl;
+        return 1;
+    }
+    char buffer[256];
+    while (fgets(buffer, sizeof(buffer), fp)) {
+        std::cout << buffer;
+    }
+
+    pclose(fp);
+
+    return 0;
+}
