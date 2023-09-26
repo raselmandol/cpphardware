df8d3bbb3216d58ccdd2498fef36ad3144476aeb
Md. Rasel Mandol <130071257+raselmandol@users.noreply.github.com>
2023-09-26 21:53:54 +0530

    Create Windows.cpp

---
+++ DetailedAudio/Windows.cpp
@@ -0,0 +1,25 @@
+//My time has come
+//living the last moments
+
+#include <iostream>
+#include <Windows.h>
+
+int main() {
+    int numDevices = waveInGetNumDevs();
+    std::cout << "Input Audio Devices:" << std::endl;
+    for (int i = 0; i < numDevices; ++i) {
+        WAVEINCAPS deviceInfo;
+        if (waveInGetDevCaps(i, &deviceInfo, sizeof(deviceInfo)) == MMSYSERR_NOERROR) {
+            std::cout << "Device " << i << ": " << deviceInfo.szPname << std::endl;
+        }
+    }
+    std::cout << "Output Audio Devices:" << std::endl;
+    numDevices = waveOutGetNumDevs();
+    for (int i = 0; i < numDevices; ++i) {
+        WAVEOUTCAPS deviceInfo;
+        if (waveOutGetDevCaps(i, &deviceInfo, sizeof(deviceInfo)) == MMSYSERR_NOERROR) {
+            std::cout << "Device " << i << ": " << deviceInfo.szPname << std::endl;
+        }
+    }
+    return 0;
+}
