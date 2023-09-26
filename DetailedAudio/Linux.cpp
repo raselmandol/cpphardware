4afa2b66f045b17607284b677e3db3a6e097a3ba
Md. Rasel Mandol <130071257+raselmandol@users.noreply.github.com>
2023-09-26 21:54:11 +0530

    Create Linux.cpp

---
+++ DetailedAudio/Linux.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <alsa/asoundlib.h>
+
+int main() {
+    snd_ctl_t *handle;
+    snd_ctl_card_info_t *info;
+    int card = -1;
+
+    while (snd_card_next(&card) >= 0 && card >= 0) {
+        char cardName[32];
+        sprintf(cardName, "hw:%d", card);
+
+        if (snd_ctl_open(&handle, cardName, 0) < 0) {
+            continue;
+        }
+
+        snd_ctl_card_info_alloca(&info);
+        snd_ctl_card_info(handle, info);
+
+        std::cout << "Card " << card << ": " << snd_ctl_card_info_get_name(info) << std::endl;
+        snd_ctl_close(handle);
+    }
+
+    return 0;
+}
