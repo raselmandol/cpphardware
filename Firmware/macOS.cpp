bffc13b7deef59b84d54c09277bb169931c4408a
Md. Rasel Mandol <130071257+raselmandol@users.noreply.github.com>
2023-09-26 21:57:02 +0530

    Create macOS.cpp

---
+++ Firmware/macOS.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cstdio>
+
+int main() {
+    FILE* fp = popen("system_profiler SPHardwareDataType", "r");
+    if (!fp) {
+        std::cerr << "Failed to execute system_profiler" << std::endl;
+        return 1;
+    }
+
+    char buffer[256];
+    while (fgets(buffer, sizeof(buffer),
