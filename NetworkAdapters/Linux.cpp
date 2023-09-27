5069019eb2feb6a722bb67d9bb223d34da6038b3
Md. Rasel Mandol <130071257+raselmandol@users.noreply.github.com>
2023-09-27 23:15:21 +0530

    Create Linux.cpp

---
+++ NetworkAdapters/Linux.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstdio>
+
+int main() {
+    FILE* fp = popen("ifconfig", "r");
+    if (!fp) {
+        std::cerr << "Failed to execute ifconfig" << std::endl;
+        return 1;
+    }
+    char buffer[256];
+    while (fgets(buffer, sizeof(buffer), fp)) {
+        std::cout << buffer;
+    }
+    pclose(fp);
+
+    return 0;
+}
