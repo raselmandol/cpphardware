87734fda3e15c14b0d8710a50d031bf7d61335b5
Md. Rasel Mandol <130071257+raselmandol@users.noreply.github.com>
2023-09-30 20:53:59 +0530

    Create Linux.cpp

---
+++ DetailedEFI/Linux.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstdio>
+
+int main() {
+    FILE* pipe = popen("dmidecode", "r");
+    if (!pipe) {
+        std::cerr << "Failed to run dmidecode" << std::endl;
+        return 1;
+    }
+
+    char buffer[128];
+    while (fgets(buffer, sizeof(buffer), pipe) != nullptr) {
+        std::cout << buffer;
+    }
+    pclose(pipe);
+    return 0;
+}
