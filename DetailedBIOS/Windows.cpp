931b20e48a6bea598e821af85c027b9914158eaa
Md. Rasel Mandol <130071257+raselmandol@users.noreply.github.com>
2023-09-30 20:51:39 +0530

    Create Windows.cpp

---
+++ DetailedBIOS/Windows.cpp
@@ -0,0 +1,120 @@
+#include <iostream>
+#include <windows.h>
+#include <wbemidl.h>
+
+int main() {
+    HRESULT hres;
+    IWbemLocator* pLoc = nullptr;
+    IWbemServices* pSvc = nullptr;
+    IEnumWbemClassObject* pEnumerator = nullptr;
+    IWbemClassObject* pclsObj = nullptr;
+    ULONG uReturn;
+    hres = CoInitializeSecurity(
+        nullptr,
+        -1,
+        nullptr,
+        nullptr,
+        RPC_C_AUTHN_LEVEL_DEFAULT,
+        RPC_C_IMP_LEVEL_IMPERSONATE,
+        nullptr,
+        EOAC_NONE,
+        nullptr
+    );
+
+    if (FAILED(hres)) {
+        std::cerr << "Failed to initialize COM security" << std::endl;
+        return 1;
+    }
+    hres = CoInitializeEx(0, COINIT_MULTITHREADED);
+    if (FAILED(hres)) {
+        std::cerr << "Failed to initialize COM library" << std::endl;
+        CoUninitialize();
+        return 1;
+    }
+    hres = CoCreateInstance(
+        CLSID_WbemLocator,
+        0,
+        CLSCTX_INPROC_SERVER,
+        IID_IWbemLocator,
+        reinterpret_cast<LPVOID*>(&pLoc)
+    );
+
+    if (FAILED(hres)) {
+        std::cerr << "Failed to create IWbemLocator object" << std::endl;
+        CoUninitialize();
+        return 1;
+    }
+    hres = pLoc->ConnectServer(
+        _bstr_t(L"ROOT\\CIMV2"),
+        nullptr,
+        nullptr,
+        0,
+        nullptr,
+        0,
+        0,
+        &pSvc
+    );
+
+    if (FAILED(hres)) {
+        std::cerr << "Failed to connect to WMI namespace" << std::endl;
+        pLoc->Release();
+        CoUninitialize();
+        return 1;
+    }
+    hres = CoSetProxyBlanket(
+        pSvc,
+        RPC_C_AUTHN_WINNT,
+        RPC_C_AUTHZ_NONE,
+        nullptr,
+        RPC_C_AUTHN_LEVEL_CALL,
+        RPC_C_IMP_LEVEL_IMPERSONATE,
+        nullptr,
+        EOAC_NONE
+    );
+
+    if (FAILED(hres)) {
+        std::cerr << "Failed to set proxy blanket" << std::endl;
+        pSvc->Release();
+        pLoc->Release();
+        CoUninitialize();
+        return 1;
+    }
+    hres = pSvc->ExecQuery(
+        bstr_t("WQL"),
+        bstr_t("SELECT * FROM Win32_BIOS"),
+        WBEM_FLAG_FORWARD_ONLY | WBEM_FLAG_RETURN_IMMEDIATELY,
+        nullptr,
+        &pEnumerator
+    );
+
+    if (FAILED(hres)) {
+        std::cerr << "Failed to execute query" << std::endl;
+        pSvc->Release();
+        pLoc->Release();
+        CoUninitialize();
+        return 1;
+    }
+
+    while (pEnumerator) {
+        hres = pEnumerator->Next(WBEM_INFINITE, 1, &pclsObj, &uReturn);
+        if (0 == uReturn) {
+            break;
+        }
+
+        VARIANT vtProp;
+        CIMTYPE pType;
+        hres = pclsObj->Get(L"Manufacturer", 0, &vtProp, &pType, 0);
+        if (SUCCEEDED(hres)) {
+            std::wcout << L"Manufacturer: " << vtProp.bstrVal << std::endl;
+            VariantClear(&vtProp);
+        }
+
+        pclsObj->Release();
+    }
+    pSvc->Release();
+    pLoc->Release();
+    pEnumerator->Release();
+    CoUninitialize();
+
+    return 0;
+}
