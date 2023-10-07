c81d5c8b8e3e8cfe770316292d2c62105cd0b540
Md. Rasel Mandol <130071257+raselmandol@users.noreply.github.com>
2023-10-07 20:47:16 +0530

    Create Windows.cpp

---
+++ DetailedScreens/Windows.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <windows.h>
+
+int main() {
+    int numScreens = GetSystemMetrics(SM_CMONITORS);
+
+    for (int i = 0; i < numScreens; ++i) {
+        DISPLAY_DEVICE displayDevice;
+        displayDevice.cb = sizeof(DISPLAY_DEVICE);
+        if (EnumDisplayDevices(nullptr, i, &displayDevice, 0)) {
+            std::cout << "Screen " << i << ":\n";
+            std::cout << "Device Name: " << displayDevice.DeviceName << std::endl;
+            std::cout << "Device String: " << displayDevice.DeviceString << std::endl;
+            std::cout << std::endl;
+        }
+    }
+
+    return 0;
+}
