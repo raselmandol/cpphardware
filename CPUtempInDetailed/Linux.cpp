8a642ad4feecea0dd9ad2ffb5d84e69e4b3b8e27
Md. Rasel Mandol <130071257+raselmandol@users.noreply.github.com>
2023-10-12 23:37:06 +0530

    Create Linux.cpp

---
+++ CPUtempInDetailed/Linux.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+int main() {
+    std::ifstream temperatureFile("/sys/class/hwmon/hwmon0/temp1_input");
+    if (temperatureFile) {
+        int temperature;
+        temperatureFile >> temperature;
+        temperatureFile.close();
+        double tempCelsius = static_cast<double>(temperature) / 1000;
+        std::cout << "CPU Temperature: " << tempCelsius << "°C" << std::endl;
+    } else {
+        std::cerr << "Unable to open temperature file." << std::endl;
+        return 1;
+    }
+
+    return 0;
+}
