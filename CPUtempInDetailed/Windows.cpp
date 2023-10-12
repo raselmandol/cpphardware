3a00f7338c2b46d2f50e8e5bf12e092c03d787ed
Md. Rasel Mandol <130071257+raselmandol@users.noreply.github.com>
2023-10-12 23:36:43 +0530

    Create Windows.cpp

---
+++ CPUtempInDetailed/Windows.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <comdef.h>
+#include <WbemIdl.h>
+#pragma comment(lib, "wbemuuid.lib")
+
+int main() {
+    CoInitialize(nullptr);
+
+    IWbemLocator* pLoc = nullptr;
+    IWbemServices* pSvc = nullptr;
+
+    HRESULT hres = CoCreateInstance(CLSID_WbemLocator, 0, CLSCTX_INPROC_SERVER, IID_IWbemLocator, (LPVOID*)&pLoc);
+
+    if (SUCCEEDED(hres)) {
+        hres = pLoc->ConnectServer(_bstr_t(L"ROOT\\WMI"), nullptr, nullptr, 0, nullptr, 0, 0, &pSvc);
+
+        if (SUCCEEDED(hres)) {
+            hres = CoSetProxyBlanket(pSvc, RPC_C_AUTHN_WINNT, RPC_C_AUTHZ_NONE, nullptr, RPC_C_AUTHN_LEVEL_CALL, RPC_C_IMP_LEVEL_IMPERSONATE, nullptr, EOAC_NONE);
+
+            if (SUCCEEDED(hres)) {
+                IEnumWbemClassObject* pEnumerator = nullptr;
+                hres = pSvc->ExecQuery(bstr_t("WQL"), bstr_t("SELECT * FROM MSAcpi_ThermalZoneTemperature"), WBEM_FLAG_FORWARD_ONLY, nullptr, &pEnumerator);
+
+                if (SUCCEEDED(hres)) {
+                    IWbemClassObject* pclsObj = nullptr;
+                    ULONG uReturn = 0;
+
+                    while (pEnumerator) {
+                        HRESULT hr = pEnumerator->Next(WBEM_INFINITE, 1, &pclsObj, &uReturn);
+
+                        if (0 == uReturn) {
+                            break;
+                        }
+
+                        VARIANT vtProp;
+                        hr = pclsObj->Get(L"CurrentTemperature", 0, &vtProp, 0, 0);
+                        if (SUCCEEDED(hr)) {
+                            int temperature = vtProp.intVal / 10 - 273;
+                            std::wcout << "CPU Temperature: " << temperature << "°C" << std::endl;
+                            VariantClear(&vtProp);
+                        }
+                        pclsObj->Release();
+                    }
+                    pEnumerator->Release();
+                }
+            }
+            pSvc->Release();
+        }
+        pLoc->Release();
+    }
+
+    CoUninitialize();
+
+    return 0;
+}
