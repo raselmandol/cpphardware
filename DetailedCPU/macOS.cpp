3f069639e7b30a92bcfa422f087f3efa9f0b8195
Md. Rasel Mandol <130071257+raselmandol@users.noreply.github.com>
2023-10-19 22:06:29 +0530

    Update macOS.cpp

--- DetailedCPU/macOS.cpp
+++ DetailedCPU/macOS.cpp
@@ -5,6 +5,7 @@ int main() {
     //running 'sysctl' to get CPU details
     //this one I got as the largest ref of hardware with c++
     //Jokes23
+    //system comments 
     std::system("sysctl -a | grep machdep.cpu");
 
     return 0;
