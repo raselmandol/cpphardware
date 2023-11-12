28d7564e49303be89d588bd504dba4a6afd71b69
Md. Rasel Mandol <130071257+raselmandol@users.noreply.github.com>
2023-11-12 22:25:39 +0530

    Create macOS.cpp

---
+++ BIOSdump/macOS.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstdio>
+
+int main() {
+    //Get BIOS information on macOS is not straightforward; you may use system_profiler for hardware info
+    FILE* pipe = popen("system_profiler SPHardwareDataType", "r");
+    if (pipe) {
+        char buffer[128];
+        while (fgets(buffer, sizeof(buffer), pipe) != nullptr) {
+            std::cout << buffer;
+        }
+        pclose(pipe);
+    }
+
+    return 0;
+}
