31ac265901ff8e936c01718828fd34ffe9dcb710
Md. Rasel Mandol <130071257+raselmandol@users.noreply.github.com>
2023-11-14 21:31:21 +0530

    Create Windows.cpp

---
+++ GPUtemp/Windows.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <Windows.h>
+#include <Ntddvdeo.h>
+
+int main() {
+    ADAPTER_INFO adapterInfo;
+    DISPLAYCONFIG_VIDEO_SIGNAL_INFO signalInfo;
+
+    memset(&adapterInfo, 0, sizeof(adapterInfo));
+    adapterInfo.Size = sizeof(adapterInfo);
+    memset(&signalInfo, 0, sizeof(signalInfo));
+    signalInfo.sizeOfStruct = sizeof(DISPLAYCONFIG_VIDEO_SIGNAL_INFO);
+
+    if (EnumDisplayDevices(nullptr, 0, &adapterInfo, 0)) {
+        if (GetDisplayConfigBufferSizes(QDC_DATABASE_CURRENT, &numPathArrayElements, &numModeInfoArrayElements) == ERROR_SUCCESS) {
+            auto pathArray = new DISPLAYCONFIG_PATH_INFO[numPathArrayElements];
+            auto modeInfoArray = new DISPLAYCONFIG_MODE_INFO[numModeInfoArrayElements];
+
+            if (QueryDisplayConfig(QDC_DATABASE_CURRENT, &numPathArrayElements, pathArray, &numModeInfoArrayElements, modeInfoArray, nullptr) == ERROR_SUCCESS) {
+                for (UINT32 i = 0; i < numPathArrayElements; ++i) {
+                    if (pathArray[i].sourceInfo.adapterId.HighPart == adapterInfo.AdapterId.HighPart &&
+                        pathArray[i].sourceInfo.adapterId.LowPart == adapterInfo.AdapterId.LowPart) {
+                        signalInfo = modeInfoArray[pathArray[i].sourceInfo.modeInfoIdx].targetVideoSignalInfo;
+                        break;
+                    }
+                }
+            }
+            delete[] pathArray;
+            delete[] modeInfoArray;
+        }
+    }
+
+    std::cout << "GPU Temperature: " << signalInfo.scanLineStartTime / 1000.0 << " degrees Celsius" << std::endl;
+
+    return 0;
+}
