e55a2e0936dcdd7775bc20b308dda53307156feb
Md. Rasel Mandol <130071257+raselmandol@users.noreply.github.com>
2024-01-30 19:57:36 +0530

    Update Linux.cpp

--- BIOSdump/Linux.cpp
+++ BIOSdump/Linux.cpp
@@ -4,7 +4,7 @@
 
 int main() {
     //Get BIOS information on Linux can be challenging; often you may need to rely on third-party tools
-    //For hardware information, you can use /proc filesystem
+    //use /proc filesystem
     std::ifstream cpuInfo("/proc/cpuinfo");
     std::string line;
     while (std::getline(cpuInfo, line)) {
