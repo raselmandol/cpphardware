c6437cdd26b87288d73191cf6d8ab4db8e10502d
Md. Rasel Mandol <130071257+raselmandol@users.noreply.github.com>
2024-01-30 19:57:46 +0530

    Update Windows.cpp

--- BIOSdump/Windows.cpp
+++ BIOSdump/Windows.cpp
@@ -8,7 +8,7 @@ int main() {
     TCHAR biosInfo[4096];
     GetFirmwareEnvironmentVariable(_T("SystemBiosDate"), _T("{00000000-0000-0000-0000-000000000000}"), biosInfo, sizeof(biosInfo));
 
-    //Get hardware information using WMI
+    //hardware information using WMI
     std::system("wmic cpu get caption");
     std::system("wmic memorychip get capacity");
     //Add more WMI queries for additional hardware information as needed
