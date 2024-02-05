c91247fa9c6bfd310488def659adeca85d71f2f7
Md. Rasel Mandol <130071257+raselmandol@users.noreply.github.com>
2024-02-05 21:47:11 +0530

    Update Linux.cpp

--- DetailedAudio/Linux.cpp
+++ DetailedAudio/Linux.cpp
@@ -16,6 +16,7 @@ int main() {
 
         snd_ctl_card_info_alloca(&info);
         snd_ctl_card_info(handle, info);
+        //printing infos
 
         std::cout << "Card " << card << ": " << snd_ctl_card_info_get_name(info) << std::endl;
         snd_ctl_close(handle);
