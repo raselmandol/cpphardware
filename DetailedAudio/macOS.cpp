4ea6878080a9c0c730bd23f9984c83efe9d63a11
Md. Rasel Mandol <130071257+raselmandol@users.noreply.github.com>
2024-02-05 21:47:45 +0530

    Update macOS.cpp

--- DetailedAudio/macOS.cpp
+++ DetailedAudio/macOS.cpp
@@ -12,13 +12,14 @@ int main() {
 
     status = AudioObjectGetPropertyDataSize(kAudioObjectSystemObject, &propertyAddress, 0, NULL, &propertySize);
     if (status == noErr) {
+        //audio_device IDs
         AudioDeviceID *deviceIDs = new AudioDeviceID[propertySize / sizeof(AudioDeviceID)];
 
         status = AudioObjectGetPropertyData(kAudioObjectSystemObject, &propertyAddress, 0, NULL, &propertySize, deviceIDs);
         if (status == noErr) {
             for (UInt32 i = 0; i < propertySize / sizeof(AudioDeviceID); ++i) {
                 deviceID = deviceIDs[i];
-                // Retrieve and print device information as needed.
+                //retrieve and print device information as needed.
             }
         }
 
